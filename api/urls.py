449043408ab0978caee99df75c9a76d8ab2b32a7
nemzmen <nemz@o2.com>
2021-02-12 19:43:03 +0100

    added simulation endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import NoiseView
+from .noise.views import NoiseView
+from .simulation.views import SimulationView
 
 
 urlpatterns = [
   path('noise', NoiseView.as_view()),
+  path('simulation', SimulationView.as_view()),
 ]
