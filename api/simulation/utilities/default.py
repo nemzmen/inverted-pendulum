a7ae9f1bcc0b465e1fd1cee90bb85cfc7b24c9b5
nemzmen <nemz@o2.com>
2021-02-13 21:25:07 +0100

    added default parametrs file

---
+++ api/simulation/utilities/default.py
@@ -0,0 +1,18 @@
+simulation_time = 40            # sekundy
+sampling_time = 0.05            # sekundy
+timeout = 0                     # współczynnik
+
+pendulum_mass = 1               # kilogramy
+pendulum_length = 0.5           # metry
+pendulum_friction = 0.1         # współczynnik
+
+proportional = 1                # współczynnik
+integral = 1                    # współczynnik
+derivative = 1                  # współczynnik
+
+output_signal = 0               # radiany
+pendulum_speed = 0              # radiany na sekundę
+pendulum_acceleration = 0       # radiany na sekundę do kwadratu
+
+setpoint = 0                    # radian
+gravity_acceleration = 9.80665  # metry na sekundę do kwadratu
