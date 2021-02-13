e2be87bde7bcd1f9519605951e2992faff6744e0
Natalia631 <56476732+Natalia631@users.noreply.github.com>
2021-02-13 16:36:15 +0100

    PID after tuning

--- api/simulation/utilities/input.py
+++ api/simulation/utilities/input.py
@@ -1,13 +1,15 @@
-t_symu = 3         #czas symulacji
-Tp = 0.001             #okres próbkowania
+t_symu = 100         #czas symulacji
+Tp = 0.05           #okres próbkowania
 m = 0.2             #masa wahadła
 I = 0.006           #bezwładność wahadła
-l = 0.3             #długość wahadła
+l = 0.6             #długość wahadła
 f = 0.1             #współczynnik tarcia
 Pp = 1              #nastawa regulatora P
+xz = 0             #kąt zadany
+
 #PID-----------------------------------------
 Kp = 1
-Ki = 0.6
-Kd = 0.001
+Ki = 0.001
+Kd = 0.11
 
-Mz= 0                   #moement zakłócający
+Mz= 0                #moement zakłócający
