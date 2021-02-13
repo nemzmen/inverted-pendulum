1179d3ba223674277f0186d085663107fa2061d6
nemzmen <nemz@o2.com>
2021-02-13 21:24:16 +0100

    added getSignal function

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -1,16 +1,35 @@
-import urls from './urls';
-import methods from './methods';
 import headers from './headers';
+import methods from './methods';
+import urls from './urls';
 
 
-export const getNoiseSignal = async ({size, mean, variance, noiseType}) => {
+export const getNoiseData = async ({size, mean, variance, noiseType}) => {
   const url = urls.host + urls.noise;
   const requestOptions = {
     method: methods.post,
     headers: headers.default,
     body: JSON.stringify({size, mean, variance, noise_type: noiseType})
   }
+  try {
+    const response = await fetch(url, requestOptions);
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    const data = await response.json();
+    return {data};
+    
+  } catch (error) {
+    return {error};
+  }
+}
 
+export const getSimulationData = async (data) => {
+  const url = urls.host + urls.simulation;
+  const requestOptions = {
+    method: methods.post,
+    headers: headers.default,
+    body: JSON.stringify(data)
+  }
   try {
     const response = await fetch(url, requestOptions);
     if (!response.ok) {
