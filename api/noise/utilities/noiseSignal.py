94d3d62cecc5b6fd3cf537ef6d7135026f485857
nemzmen <nemz@o2.com>
2021-02-14 01:52:00 +0100

    added sin noise signal

--- api/noise/utilities/noiseSignal.py
+++ api/noise/utilities/noiseSignal.py
@@ -1,44 +1,43 @@
+import math
+
 import numpy
 
 
-noise_types = {
-    'normal': 'NORMAL',
-    'pareto': 'PARETO',
-    'sum': 'SUM',
-}
-
-
 class NoiseSignal:
-    def __init__(self):
-        self.sum = 'sum'
-        self.step = 'step'
-        self.value = 'value'
-        self.normal = 'normal'
-        self.pareto = 'pareto'
+    step = 'step'
+    value = 'value'
+    NORMAL = 'NORMAL'
+    PARETO = 'PARETO'
+    SIN = 'SIN'
+    SUM = 'SUM'
+    default_size = 100
+    default_mean = 0.0
+    default_variance = 1.0
+    default_noise_type = NORMAL
 
-        self.size = 100
-        self.mean = 0.0
-        self.variance = 1.0
-        self.noise_type = noise_types['normal']
 
-    def get_noise(self, mean=None, size=None, variance=None, noise_type=None):
-        size = self.size if size == None else size
-        mean = self.mean if mean == None else mean
-        variance = self.variance if variance == None else variance
-        noise_type = self.noise_type if noise_type == None else noise_type
+    def get_noise(self, size=None, mean=None, variance=None, noise_type=None):
+        size = self.default_size if size == None else size
+        mean = self.default_mean if mean == None else mean
+        variance = self.default_variance if variance == None else variance
+        noise_type = self.default_noise_type if noise_type == None else noise_type
 
-        data_array = []
+        noise_signal = []
 
-        if noise_type == noise_types[self.normal]:
-            data_array = numpy.random.normal(loc=mean, scale=variance, size=size)
+        if noise_type == self.NORMAL:
+            noise_signal = numpy.random.normal(loc=mean, scale=variance / 3, size=size)
 
-        if noise_type == noise_types[self.pareto]:
-            data_array = (numpy.random.pareto(a=2.5, size=size) * variance) + mean
+        if noise_type == self.PARETO:
+            noise_signal = (numpy.random.pareto(a=2.5, size=size) * variance / 3) + mean
+
+        if noise_type == self.SIN:
+            noise_signal = (numpy.sin(numpy.linspace(0, 4*numpy.pi, size)) * variance) + mean
         
-        if noise_type == noise_types[self.sum]:
-            normal_signal = numpy.random.normal(loc=mean, scale=variance, size=size)
-            pareto_signal = (numpy.random.pareto(a=4, size=size) + mean) * variance
-            data_array = numpy.add(normal_signal,pareto_signal)
+        if noise_type == self.SUM:
+            normal_signal = numpy.random.normal(loc=mean, scale=variance / 3, size=size)
+            pareto_signal = (numpy.random.pareto(a=4, size=size) * variance / 3) + mean
+            sinus_signal = (numpy.sin(numpy.linspace(0, 4*numpy.pi, size)) * variance) + mean
+            normal_pareto_signal = numpy.add(normal_signal, pareto_signal)
+            noise_signal = numpy.add(normal_pareto_signal, sinus_signal)
 
-        noise_signal = [{self.step: index, self.value: item} for index, item in enumerate(data_array)]
-        return noise_signal
+        return noise_signal.tolist()
--- frontend/static/dictionaries/index.js
+++ frontend/static/dictionaries/index.js
@@ -1,5 +1,6 @@
 export const noiseTypes = {
   normal: 'NORMAL',
   pareto: 'PARETO',
+  sin: 'SIN',
   sum: 'SUM',
 }
