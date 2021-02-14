9e88542888d07569b644537dc8222b42d86bb97b
nemzmen <nemz@o2.com>
2021-02-14 01:52:11 +0100

    added noise endpoint

--- frontend/src/api/endpoints.js
+++ frontend/src/api/endpoints.js
@@ -3,12 +3,12 @@ import methods from './methods';
 import urls from './urls';
 
 
-export const getNoiseData = async ({size, mean, variance, noiseType}) => {
+export const getNoiseData = async (data) => {
   const url = urls.host + urls.noise;
   const requestOptions = {
     method: methods.post,
     headers: headers.default,
-    body: JSON.stringify({size, mean, variance, noise_type: noiseType})
+    body: JSON.stringify(data)
   }
   try {
     const response = await fetch(url, requestOptions);
