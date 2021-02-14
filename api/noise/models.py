f77cf1ea558c2dcad9e62ac8426fc9a3912fd2f5
nemzmen <nemz@o2.com>
2021-02-14 01:51:20 +0100

    added sampling time to noise

--- api/noise/models.py
+++ api/noise/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class NoiseModel(models.Model):
     array = models.JSONField(null=True)
+    sampling_time = models.FloatField(null=True, default=0.05)
     mean = models.FloatField(null=True, default=0.0)
     variance = models.FloatField(null=True, default=1.0)
     noise_type = models.CharField(max_length=16, default='')
