c0c1e05551513d1e0f4745eb3d6897dc77789e7d
nemzmen <nemz@o2.com>
2021-02-14 01:52:50 +0100

    added quality parameters

--- api/noise/views.py
+++ api/noise/views.py
@@ -1,58 +1,64 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from ..helpers import check_float
 from .models import NoiseModel
 from .serializers import NoiseSerializer
 from .utilities.noiseSignal import NoiseSignal
-from ..helpers import check_int, check_float
 
 
 class NoiseView(APIView):
+
     serializer_class = NoiseSerializer
-    size = 'size'
+    simulation_time = 'simulation_time'
+    sampling_time = 'sampling_time'
     mean = 'mean'
     signal = 'signal'
     variance = 'variance'
     noise_type = 'noise_type'
 
     def post(self, request, format=None):
-        queryset = NoiseModel.objects.all()
-        readed_variable_size = request.data.get(self.size, None)
+        readed_variable_simulation_time = request.data.get(self.simulation_time, None)
+        readed_variable_sampling_time = request.data.get(self.sampling_time, None)
         readed_variable_mean = request.data.get(self.mean, None)
         readed_variable_variance = request.data.get(self.variance, None)
         readed_variable_noise_type = request.data.get(self.noise_type, None)
 
-        if not check_int(readed_variable_size):
-            return Response({'Type error': 'cannot convert size to int'}, status=status.HTTP_400_BAD_REQUEST)
-
+        if not check_float(readed_variable_simulation_time):
+            return Response({'Type error': 'cannot convert simulation_time to float'}, status=status.HTTP_400_BAD_REQUEST)
+        if not check_float(readed_variable_sampling_time):
+            return Response({'Type error': 'cannot convert sampling_time to float'}, status=status.HTTP_400_BAD_REQUEST)
         if not check_float(readed_variable_mean):
             return Response({'Type error': 'cannot convert mean to float'}, status=status.HTTP_400_BAD_REQUEST)
-
         if not check_float(readed_variable_variance):
             return Response({'Type error': 'cannot convert variance to float'}, status=status.HTTP_400_BAD_REQUEST)
-
         if type(readed_variable_noise_type) != str:
             return Response({'Type error': 'noise_type must be a string'}, status=status.HTTP_400_BAD_REQUEST)
         
-        size = int(readed_variable_size)
+        simulation_time = float(readed_variable_simulation_time)
+        sampling_time = float(readed_variable_sampling_time)
         mean = float(readed_variable_mean)
         variance = float(readed_variable_variance)
         noise_type = readed_variable_noise_type
 
+        size = int(simulation_time / sampling_time)
         noise_signal = NoiseSignal().get_noise(size=size, mean=mean, variance=variance, noise_type=noise_type)
         array={self.signal: noise_signal}
 
-        if len(queryset) == 0:
-            noise = NoiseModel(array=array, mean=mean, variance=variance, noise_type=noise_type)
-            noise.save()
-            return Response(self.serializer_class(noise).data, status=status.HTTP_201_CREATED)
+        queryset = NoiseModel.objects.all()
 
-        else:
+        if len(queryset) > 0:
             noise = queryset.first()
             noise.array = array
+            noise.sampling_time = sampling_time
             noise.mean = mean
             noise.variance = variance
             noise.noise_type = noise_type
             noise.save()
             return Response(self.serializer_class(noise).data, status=status.HTTP_200_OK)
+
+        noise = NoiseModel(array=array, sampling_time=sampling_time, mean=mean, variance=variance, noise_type=noise_type)
+        noise.save()
+        return Response(self.serializer_class(noise).data, status=status.HTTP_201_CREATED)
--- api/simulation/utilities/simulation.py
+++ api/simulation/utilities/simulation.py
@@ -1,6 +1,7 @@
 import math
 import random
 
+from ...noise.models import NoiseModel
 from . import default
 from .fuzzyController import FuzzyController
 from .invertedPendulum import InvertedPendulum
@@ -9,6 +10,7 @@ from .proportionalController import ProportionalController
 
 
 class Simulation:
+    signal = 'signal'
 
     def __init__(self, simulation_time=None, sampling_time=None, timeout=None, pendulum_mass=None, pendulum_length=None):
         self.simulation_time = default.simulation_time if simulation_time == None else simulation_time
@@ -24,27 +26,60 @@ class Simulation:
     def radian_to_degree(self, radian):
         return radian * 180 / math.pi
 
+    
+    def get_noise_signal(self, length):
+        queryset = NoiseModel.objects.all()
+        if len(queryset) == 0:
+            return [0] * length
+
+        output_signal = []
+        noise = queryset.first()
+        signal = noise.array[self.signal]
+        ratio = len(signal) / length
+
+        for i in range(length):
+            signal_index = int(i * ratio)
+            output_signal.append(signal[signal_index])
+
+        return output_signal
+
 
     def get_simulation_data(self, pendulum, controller):
+        error_abs = 0
+        error_square = 0
+        control_abs = 0
+        control_square = 0
         signal = []
+
         output_signal = default.output_signal
         control_signal_register = [0] * max(self.timeout, 1)
+        noise_signal = self.get_noise_signal(self.data_length)
 
-        for _ in range(self.data_length):
-            signal.append(self.radian_to_degree(output_signal))                     # dodanie kąta wychylenia do tablicy
+        for i in range(self.data_length):
+            # dodanie danych do tablicy
+            signal.append(self.radian_to_degree(output_signal))
+
+            # obliczenia symulacji
             error_signal = default.setpoint - output_signal                         # uchyb sygnału (xe)
-
             control_signal = controller.get_output(error_signal)                    # wyjście regulatora (xs)
             control_signal_register.pop()                                           # usunięcie ostatniego elementu rejestru
             control_signal_register.insert(0, control_signal)                       # dodanie elementu do rejestru
             delayed_control_signal = control_signal_register[-1]                    # pobranie ostatniego elementu rejestru
-
             control_moment = math.sin(delayed_control_signal) * self.mgl_ratio      # moment sterujący (Ms)
-            noise_moment = (random.random() - 0.5) / 10                             # moment zakłócający (Mz) (pobrać z bazy danych)
-
+            noise_moment = noise_signal[i]                                          # moment zakłócający (Mz)
             external_moment = control_moment + noise_moment                         # suma momentów zewnętrznych
             output_signal = pendulum.get_output(external_moment)                    # kąt wychylenia
 
-            # wyliczyc tutaj parametry jakosci i zwrócić dodatkowo w dict
+            # wyliczenie parametrów jakości
+            error_abs += abs(error_signal) * self.sampling_time
+            error_square += (error_signal ** 2) * self.sampling_time
+            control_abs += abs(control_moment) * self.sampling_time
+            control_square += (control_moment ** 2) * self.sampling_time
 
-        return dict(signal=signal)
+        return dict(
+                signal=signal,
+                error_abs=error_abs,
+                error_square=error_square,
+                control_abs=control_abs,
+                control_square=control_square)
+                
